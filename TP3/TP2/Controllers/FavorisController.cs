3442792343c1d51724bd59c0d8cf06401ebb1ee7
2568832 <2568832@cegepmontpetit.ca>
2026-05-08 16:40:28 -0400

    QUOINCé a Ajouter enfant

--- TP3/TP2/Controllers/FavorisController.cs
+++ TP3/TP2/Controllers/FavorisController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using TP2.Extensions;
 using TP2.Models;
+
 
 
 namespace TP2.Controllers
@@ -16,18 +20,79 @@ namespace TP2.Controllers
         [Route("/Favoris/Index")]
         public IActionResult Index()
         {
-            return View("Index", _baseDeDonnees.Carte_Graphiques.ToList());
-        }
-      
+            var enfantIDs = HttpContext.Session.Get<List<int>>("enfantIDs");
+            if (enfantIDs == null)
+            {
+                enfantIDs= new List<int>();
+            }
 
+            var enfantsDeLaBD = _baseDeDonnees.Carte_Graphiques.Where(e => enfantIDs.Contains(e.ID)).ToList();
+
+            return View(enfantsDeLaBD);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult AjouterUnEnfant(int id)
         {
-            return View();
+              
+
+                List<int> ID = HttpContext.Session.Get<List<int>>("enfantIDs");
+
+                if (ID == null)
+                {
+                    ID = new List<int>();
+                }
+
+                bool test = _baseDeDonnees.Carte_Graphiques.Any(c => c.ID == id);
+                if (!test)
+                {
+                    ID.Add(id);
+
+                    HttpContext.Session.Set("enfantIDs", ID);
+
+
+                    RedirectToAction("Index", "Favoris", _baseDeDonnees.Carte_Graphiques);
+                return View("Index");
+
+                 }
+                 else
+                {
+                    
+                    return View("Index");
+
+                }
+            
+
         }
+
+        
+
+        //public ActionResult Create(Crewmate collection)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return View(collection);
+
+        //    // Vérifie si la couleur existe déjà
+        //    bool colorUsed = _db.Crewmates.Any(p => p.Color == collection.Color);
+
+        //    if (colorUsed)
+        //    {
+        //        // Ajoute un message d’erreur pour l’utilisateur
+        //        ModelState.AddModelError("Color", "Cette couleur est déjà utilisée.");
+        //        return View(collection);
+        //    }
+
+        //    // Si la couleur n'est pas utilisée, on ajoute
+        //    _db.Crewmates.Add(collection);
+
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         public IActionResult SupprimerUnEnfant(int id)
         {
             return View();
         }
+
     }
 }
--- TP3/TP2/Program.cs
+++ TP3/TP2/Program.cs
@@ -16,7 +16,7 @@ namespace TP2
             builder.Services.AddSingleton<BaseDeDonnees>();
 
             var app = builder.Build();
-
+             
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
